7bf269c454bc687b43540c487a6248533fb5635c
Rinrin.rs <rinrin0413.valley@gmail.com>
2022-08-28 06:02:03 +0900

    Add:two unit tests for util module

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -18,3 +18,21 @@ pub(crate) fn max_f64(v1: f64, v2: f64) -> f64 {
         v1
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn rfc3339_to_unix_ts() {
+        let ts = "2022-07-26T17:35:23.988Z";
+        assert_eq!(to_unix_ts(ts), 1658856923);
+    }
+
+    #[test]
+    fn compare_and_return_maximum_of_two_f64() {
+        let v1 = -2.;
+        let v2 = 2.;
+        assert_eq!(max_f64(v1, v2), 2.0);
+    }
+}
