3a3b7fd2677338c9fcfa00d9e9020e9c6c5c7e02
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-06-29 08:05:57 +0900

    ✨ Add: code example to get TETRA LEAGUE stream

--- examples/ex07_get_streams/src/main.rs
+++ examples/ex07_get_streams/src/main.rs
@@ -73,6 +73,20 @@ async fn main() {
         }
     };
 
+    // User `5e32fc85ab319c2ab1beb07c`'s recent TETRA LEAGUE records stream.
+    let _stream5 = match Client::new()
+        .get_stream(
+            StreamType::League,
+            StreamContext::UserRecent,
+            Some("5e32fc85ab319c2ab1beb07c")
+        )
+        .await
+    {
+        Ok(s) => s,
+        Err(err) => {
+            panic!("Error: {}\n", err.to_string());
+        }
+    };
 
     // Learn about what we can get from following docs:
     // https://docs.rs/tetr_ch/latest/tetr_ch/model/stream/struct.StreamResponse.html
