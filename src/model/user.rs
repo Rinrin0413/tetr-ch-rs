7fa9ed77329dbefc76918a92d6d1c8774241cc0e
Rinrin.rs <rinrin0413.valley@gmail.com>
2024-11-15 19:54:07 +0900

    ✨ Add: useful methods for User Info models [#67]

--- src/model/user.rs
+++ src/model/user.rs
@@ -6,7 +6,7 @@ use crate::{
     util::{deserialize_from_non_str_to_none, max_f64, to_unix_ts},
 };
 use serde::Deserialize;
-use std::fmt::{self, Display, Formatter};
+use std::fmt;
 
 /// The response for User Info data.
 /// An object describing the user in detail.
@@ -22,245 +22,6 @@ pub struct UserResponse {
     pub cache: Option<CacheData>,
     /// The requested data.
     pub data: Option<User>,
-}
-
-impl UserResponse {
-    /// Returns the UNIX timestamp when the user's account created, if one exists.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the request was not successful.
-    pub fn account_created_at(&self) -> Option<i64> {
-        self.get_user().created_at.as_ref().map(|ts| to_unix_ts(ts))
-    }
-
-    /// Returns the level based on the user's xp.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the request was not successful.
-    pub fn level(&self) -> u32 {
-        let xp = self.get_user().xp;
-        // (xp/500)^0.6 + (xp / (5000 + max(0, xp-4000000) / 5000)) + 1
-        ((xp / 500.).powf(0.6) + (xp / (5000. + max_f64(0., xp - 4000000.) / 5000.)) + 1.).floor()
-            as u32
-    }
-
-    /// Returns the user's avatar URL.
-    /// If the user has no avatar, returns anon's.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the request was not successful.
-    pub fn face(&self) -> String {
-        let default = "https://tetr.io/res/avatar.png".to_string();
-        if let Some(ar) = self.get_user().avatar_revision {
-            if ar == 0 {
-                return default;
-            }
-            format!(
-                "https://tetr.io/user-content/avatars/{}.jpg?rv={}",
-                self.get_user().id,
-                ar
-            )
-        } else {
-            default
-        }
-    }
-
-    /// Returns the user's banner URL.
-    /// If the user has no banner, returns `None`.
-    ///
-    /// ***Even if the user is not currently a supporter,
-    /// the URL may be returned if the banner was once set.**
-    ///
-    /// # Panics
-    ///
-    /// Panics if the request was not successful.
-    pub fn banner(&self) -> Option<String> {
-        if let Some(br) = self.get_user().banner_revision {
-            if br == 0 {
-                return None;
-            }
-            Some(format!(
-                "https://tetr.io/user-content/banners/{}.jpg?rv={}",
-                self.get_user().id,
-                br
-            ))
-        } else {
-            None
-        }
-    }
-
-    /// Whether the user has at least one badge.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the request was not successful.
-    pub fn has_badges(&self) -> bool {
-        !self.get_user().badges.is_empty()
-    }
-
-    /// Whether the user is an anonymous.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the request was not successful.
-    pub fn is_anon(&self) -> bool {
-        self.get_user().role.is_anon()
-    }
-
-    /// Whether the user is a bot.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the request was not successful.
-    pub fn is_bot(&self) -> bool {
-        self.get_user().role.is_bot()
-    }
-
-    /// Whether the user is a SYSOP.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the request was not successful.
-    pub fn is_sysop(&self) -> bool {
-        self.get_user().role.is_sysop()
-    }
-
-    /// Whether the user is an administrator.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the request was not successful.
-    pub fn is_admin(&self) -> bool {
-        self.get_user().role.is_admin()
-    }
-
-    /// Whether the user is a moderator,
-    ///
-    /// # Panics
-    ///
-    /// Panics if the request was not successful.
-    pub fn is_mod(&self) -> bool {
-        self.get_user().role.is_mod()
-    }
-
-    /// Whether the user is a community moderator.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the request was not successful.
-    pub fn is_halfmod(&self) -> bool {
-        self.get_user().role.is_halfmod()
-    }
-
-    /// Whether the user is banned.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the request was not successful.
-    pub fn is_banned(&self) -> bool {
-        self.get_user().role.is_banned()
-    }
-
-    /// Whether the user is hidden.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the request was not successful.
-    pub fn is_hidden(&self) -> bool {
-        self.get_user().role.is_hidden()
-    }
-
-    /// Whether the user is bad standing.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the request was not successful.
-    pub fn is_badstanding(&self) -> bool {
-        self.get_user().is_badstanding.unwrap_or(false)
-    }
-
-    /// Whether the user is a supporter.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the request was not successful.
-    pub fn is_supporter(&self) -> bool {
-        self.get_user().is_supporter.unwrap_or(false)
-    }
-
-    /// Returns the user's profile URL.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the request was not successful.
-    pub fn profile_url(&self) -> String {
-        format!("https://ch.tetr.io/u/{}", self.get_user().username)
-    }
-
-    /// Returns an `Option<String>`.
-    ///
-    /// If user is displaying the country,
-    /// returns `Some(String)` with an image URL of the national flag based on the user's ISO 3166-1 country code.
-    /// If the user is not displaying the country, returns `None`.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the request was not successful.
-    pub fn national_flag_url(&self) -> Option<String> {
-        self.get_user()
-            .country
-            .as_ref()
-            .map(|cc| format!("https://tetr.io/res/flags/{}.png", cc.to_lowercase()))
-    }
-
-    /// Returns the badges count.
-    ///
-    /// # Panics
-    ///
-    /// Panics if the request was not successful.
-    pub fn badges_count(&self) -> usize {
-        self.get_user().badges.len()
-    }
-
-    /// Returns a UNIX timestamp when this resource was cached.
-    ///
-    /// # Panics
-    ///
-    /// Panics if there is no cache data.
-    pub fn cached_at(&self) -> i64 {
-        match self.cache.as_ref() {
-            Some(c) => c.cached_at(),
-            None => panic!("There is no cache data."),
-        }
-    }
-
-    /// Returns a UNIX timestamp when this resource's cache expires.
-    ///
-    /// # Panics
-    ///
-    /// Panics if there is no cache data.
-    pub fn cached_until(&self) -> i64 {
-        match self.cache.as_ref() {
-            Some(c) => c.cached_at(),
-            None => panic!("There is no cache data."),
-        }
-    }
-
-    /// Returns the [`&User`](crate::model::user::User).
-    ///
-    /// # Panics
-    ///
-    /// Panics if the request was not successful.
-    fn get_user(&self) -> &User {
-        if let Some(d) = self.data.as_ref() {
-            d
-        } else {
-            panic!("There is no user object because the request was not successful.")
-        }
-    }
 }
 
 impl AsRef<UserResponse> for UserResponse {
@@ -306,10 +67,10 @@ pub struct User {
     /// The user's ISO 3166-1 country code, or `None` if hidden/unknown.
     /// Some vanity flags exist.
     pub country: Option<String>,
-    /// Whether this user currently has a bad standing (recently banned).
+    /// Whether the user currently has a bad standing (recently banned).
     #[serde(rename = "badstanding")]
     pub is_badstanding: Option<bool>,
-    /// Whether this user is currently supporting TETR.IO <3
+    /// Whether the user is currently supporting TETR.IO <3
     #[serde(rename = "supporter")]
     pub is_supporter: Option<bool>, // EXCEPTION
     /// An indicator of their total amount supported,
@@ -330,9 +91,7 @@ pub struct User {
     /// This user's third party connections.
     pub connections: Connections,
     /// The amount of players who have added this user to their friends list.
-    ///
-    /// ***This field is optional but the API documentation does not mention it.**
-    pub friend_count: Option<u32>,
+    pub friend_count: Option<u32>, // EXCEPTION
     // This user's distinguishment banner.
     pub distinguishment: Option<Distinguishment>,
     /// This user's featured achievements.
@@ -347,12 +106,7 @@ pub struct User {
 }
 
 impl User {
-    /// Returns UNIX timestamp when the user's account created, if one exists.
-    pub fn account_created_at(&self) -> Option<i64> {
-        self.created_at.as_ref().map(|ts| to_unix_ts(ts))
-    }
-
-    /// Returns the level based on the user's xp.
+    /// Returns the level of the user.
     pub fn level(&self) -> u32 {
         let xp = self.xp;
         // (xp/500)^0.6 + (xp / (5000 + max(0, xp-4000000) / 5000)) + 1
@@ -360,9 +114,77 @@ impl User {
             as u32
     }
 
+    /// Returns the user's TETRA CHANNEL profile URL.
+    pub fn profile_url(&self) -> String {
+        format!("https://ch.tetr.io/u/{}", self.username)
+    }
+
+    /// Whether the user is a normal user.
+    pub fn is_normal_user(&self) -> bool {
+        self.role.is_normal_user()
+    }
+
+    /// Whether the user is an anonymous.
+    pub fn is_anon(&self) -> bool {
+        self.role.is_anon()
+    }
+
+    /// Whether the user is a bot.
+    pub fn is_bot(&self) -> bool {
+        self.role.is_bot()
+    }
+
+    /// Whether the user is a SYSOP.
+    pub fn is_sysop(&self) -> bool {
+        self.role.is_sysop()
+    }
+
+    /// Whether the user is an administrator.
+    pub fn is_admin(&self) -> bool {
+        self.role.is_admin()
+    }
+
+    /// Whether the user is a moderator.
+    pub fn is_mod(&self) -> bool {
+        self.role.is_mod()
+    }
+
+    /// Whether the user is a community moderator.
+    pub fn is_halfmod(&self) -> bool {
+        self.role.is_halfmod()
+    }
+
+    /// Whether the user is banned.
+    pub fn is_banned(&self) -> bool {
+        self.role.is_banned()
+    }
+
+    /// Whether the user is hidden.
+    pub fn is_hidden(&self) -> bool {
+        self.role.is_hidden()
+    }
+
+    /// Returns an UNIX timestamp when the user's account created.
+    ///
+    /// If the account was created before join dates were recorded, `None` is returned.
+    pub fn created_at(&self) -> Option<i64> {
+        self.created_at.as_ref().map(|ts| to_unix_ts(ts))
+    }
+
+    /// Whether the user has any badges.
+    pub fn has_badge(&self) -> bool {
+        !self.badges.is_empty()
+    }
+
+    /// Returns the number of badges the user has.
+    pub fn badge_count(&self) -> usize {
+        self.badges.len()
+    }
+
     /// Returns the user's avatar URL.
-    /// If the user has no avatar, returns anon's.
-    pub fn face(&self) -> String {
+    ///
+    /// If the user does not have an avatar, the anonymous's avatar URL is returned.
+    pub fn avatar_url(&self) -> String {
         let default = "https://tetr.io/res/avatar.png".to_string();
         if let Some(ar) = self.avatar_revision {
             if ar == 0 {
@@ -378,11 +200,13 @@ impl User {
     }
 
     /// Returns the user's banner URL.
-    /// If the user has no banner, returns `None`.
     ///
-    /// ***Even if the user is not currently a supporter,
-    /// the URL may be returned if the banner was once set.**
-    pub fn banner(&self) -> Option<String> {
+    /// If the user does not have a banner, `None` is returned.
+    ///
+    /// ***Ignore the returned value if the user is not a supporter.
+    /// Because even if the user is not currently a supporter,
+    /// `Some<String>` may be returned if the banner was once set.**
+    pub fn banner_url(&self) -> Option<String> {
         if let Some(br) = self.banner_revision {
             if br == 0 {
                 return None;
@@ -396,82 +220,13 @@ impl User {
         }
     }
 
-    /// Whether the user has at least one badge.
-    pub fn has_badge(&self) -> bool {
-        !self.badges.is_empty()
-    }
-
-    /// Whether this user is an anonymous.
-    pub fn is_anon(&self) -> bool {
-        self.role.is_anon()
-    }
-
-    /// Whether this user is a bot.
-    pub fn is_bot(&self) -> bool {
-        self.role.is_bot()
-    }
-
-    /// Whether this user is a SYSOP.
-    pub fn is_sysop(&self) -> bool {
-        self.role.is_sysop()
-    }
-
-    /// Whether this user is an administrator.
-    pub fn is_admin(&self) -> bool {
-        self.role.is_admin()
-    }
-
-    /// Whether this user is a moderator.
-    pub fn is_mod(&self) -> bool {
-        self.role.is_mod()
-    }
-
-    /// Whether this user is a community moderator.
-    pub fn is_halfmod(&self) -> bool {
-        self.role.is_halfmod()
-    }
-
-    /// Whether this user is banned.
-    pub fn is_banned(&self) -> bool {
-        self.role.is_banned()
-    }
-
-    /// Whether this user is hidden.
-    pub fn is_hidden(&self) -> bool {
-        self.role.is_hidden()
-    }
-
-    /// Whether this user is bad standing.
-    pub fn is_badstanding(&self) -> bool {
-        self.is_badstanding.unwrap_or(false)
-    }
-
-    /// Whether this user is a supporter.
-    pub fn is_supporter(&self) -> bool {
-        self.is_supporter.unwrap_or(false)
-    }
-
-    /// Returns the user's profile URL.
-    pub fn profile_url(&self) -> String {
-        format!("https://ch.tetr.io/u/{}", self.username)
-    }
-
-    /// Returns an i
-
-    /// Returns an `Option<String>`.
+    /// Returns the national flag URL of the user's country.
     ///
-    /// If user is displaying the country,
-    /// returns `Some(String)` with an image URL of the national flag based on the user's ISO 3166-1 country code.
-    /// If the user is not displaying the country, returns `None`.
+    /// If the user's country is hidden or unknown, `None` is returned.
     pub fn national_flag_url(&self) -> Option<String> {
         self.country
             .as_ref()
             .map(|cc| format!("https://tetr.io/res/flags/{}.png", cc.to_lowercase()))
-    }
-
-    /// Returns the badges count.
-    pub fn badges_count(&self) -> usize {
-        self.badges.len()
     }
 }
 
@@ -498,9 +253,7 @@ pub struct Badge {
     /// The badge's label, shown when hovered.
     pub label: String,
     /// Extra flavor text for the badge, shown when hovered.
-    ///
-    /// ***This field is optional but the API documentation does not mention it.**
-    pub desc: Option<String>,
+    pub desc: Option<String>,  // EXCEPTION
     /// The badge's timestamp, if shown.
     ///
     /// Why it uses `deserialize_with` attribute?
@@ -514,12 +267,12 @@ pub struct Badge {
 }
 
 impl Badge {
-    /// Returns the formatted badge icon URL.
+    /// Returns the badge icon URL.
     pub fn badge_icon_url(&self) -> String {
         format!("https://tetr.io/res/badges/{}.png", self.id)
     }
 
-    /// Returns a UNIX timestamp when this badge was achieved.
+    /// Returns a UNIX timestamp when the badge was achieved.
     pub fn received_at(&self) -> Option<i64> {
         self.received_at.as_ref().map(|ts| to_unix_ts(ts))
     }
@@ -679,6 +432,10 @@ pub struct UserId(pub String);
 
 impl UserId {
     /// Returns the user's internal ID.
+    #[deprecated(
+        since = "0.6.0",
+        note = "please use the `.to_string()` method instead"
+    )]
     pub fn id(&self) -> &str {
         &self.0
     }
@@ -694,7 +451,7 @@ impl UserId {
     ///
     /// Returns a [`ResponseError::HttpErr`] if the HTTP request fails.
     pub async fn get_user(&self) -> RspErr<UserResponse> {
-        Client::new().get_user(self.id()).await
+        Client::new().get_user(&self.to_string()).await
     }
 }
 
@@ -704,8 +461,8 @@ impl AsRef<UserId> for UserId {
     }
 }
 
-impl Display for UserId {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.id())
+impl fmt::Display for UserId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
     }
 }
