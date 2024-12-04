96fc77b3af94749824d013c8c85b1ad53d97822d
Rinrin.rs <rinrin0413.valley@gmail.com>
2024-12-05 03:20:21 +0900

    ✨ Add: `model::prelude` module [#15]

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,4 +1,10 @@
 //! Easy-to-use models of the various objects received from the API.
+//!
+//! You can import the models that are enums and tuple structs from the [`model::prelude`](prelude) module.
+//!
+//! ```
+//! use tetr_ch::model::prelude::*;
+//! ```
 
 #[macro_use]
 mod macros;
@@ -18,3 +24,28 @@ pub mod summary;
 pub mod user;
 pub mod user_records;
 pub mod util;
+
+/// A prelude for the models.
+///
+/// # Example
+///
+/// ```
+/// use tetr_ch::model::prelude::*;
+/// ```
+pub mod prelude {
+    pub use super::{
+        cache::Status as CacheStatus,
+        news::NewsData,
+        util::{
+            Achievement, BadgeId, Gamemode, NewsStream as NewsStreamModel, Rank,
+            RecordLeaderboard as RecordLeaderboardModel, ReplayId, Role, Timestamp, UserId,
+        },
+    };
+
+    pub(crate) use super::{
+        cache::CacheData, error_response::ErrorResponse, summary::record::Record,
+    };
+    pub(crate) use crate::client::param::pagination::Prisecter;
+    pub(crate) use serde::Deserialize;
+    pub(crate) use std::fmt;
+}
