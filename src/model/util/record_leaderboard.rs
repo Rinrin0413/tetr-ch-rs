f045834d6a4435811a777aa0840b923f4b0338bd
Rinrin.rs <rinrin0413.valley@gmail.com>
2024-12-07 17:29:25 +0900

    ✏️ Rename: `RecordLeaderboard::into_*` methods to `to_*`
    Functions that take references and return non- `impl Copy` ownerships should be named `to_*`.

--- src/model/util/record_leaderboard.rs
+++ src/model/util/record_leaderboard.rs
@@ -10,17 +10,17 @@ pub struct RecordLeaderboard(pub String);
 
 impl RecordLeaderboard {
     /// Converts into a [`crate::client::param::record_leaderboard::RecordsLeaderboardId`].
-    pub fn into_id(&self) -> RecordsLeaderboardId {
-        self._into_id(None)
+    pub fn to_id(&self) -> RecordsLeaderboardId {
+        self._to_id(None)
     }
 
     /// Converts into a [`crate::client::param::record_leaderboard::RecordsLeaderboardId`] with a Revolution ID.
-    pub fn into_id_with_revolution_id(&self, revolution_id: &str) -> RecordsLeaderboardId {
-        self._into_id(Some(revolution_id))
+    pub fn to_id_with_revolution_id(&self, revolution_id: &str) -> RecordsLeaderboardId {
+        self._to_id(Some(revolution_id))
     }
 
     /// Converts into a [`crate::client::param::record_leaderboard::RecordsLeaderboardId`] with an optional Revolution ID.
-    fn _into_id(&self, revolution_id: Option<&str>) -> RecordsLeaderboardId {
+    fn _to_id(&self, revolution_id: Option<&str>) -> RecordsLeaderboardId {
         let split_id: Vec<&str> = self.0.split('_').collect();
         let gamemode = split_id[0];
         let scope = match split_id[1] {
